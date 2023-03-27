24b995b404f6a81de23aa619aa9e8fab90d97496
chrisstruong <chrisstruong@gmail.com>
2023-03-27 13:35:42 -0400

    Finish the backend

--- controllers/people-controller.js
+++ controllers/people-controller.js
@@ -29,12 +29,19 @@ router.get("/:id", async(req, res)=> {
 })
 
 router.delete("/:id", async(req, res)=> {
-    res.status(200).json({message: "people delete route" + req.params.id})
-})
+    try {
+        res.json(await People.findByIdAndRemove(req.params.id))
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})  
 
 router.put("/:id", async(req, res)=> {
-    console.log(req.body)
-    res.status(200).json({message: "people update route" + req.params.id})
+    try{
+        res.json(await People.findByIdAndUpdate(req.params.id, req.body, {new:true}))
+    } catch(error) {
+        res.status(400).json(error)
+    }
 })
 
 module.exports = router
